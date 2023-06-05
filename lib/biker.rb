3024bd5b36ab3af33bd9b6844fc55cac83445b71
Dylan Timmons <dylanstimmons@icloud.com>
2023-06-05 10:46:07 -0600

    Fix to log_ride method

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,9 +1,26 @@
 require './lib/ride'
 
 class Biker
-    
+    attr_reader :name, :max_distance, :rides, :acceptable_terrain
+
     def initialize(name, max_distance)
         @name = name
         @max_distance = max_distance
+        @rides = {}
+        @acceptable_terrain = []
+    end
+
+    def learn_terrain!(terrain)
+        @acceptable_terrain << terrain
+    end
+
+    def log_ride(ride, time)
+        return unless acceptable_terrain.include?(ride.terrain) && ride.total_distance <= max_distance
+      
+        if rides[ride].nil?
+          rides[ride] = [time]
+        else
+          rides[ride] << time
+        end
     end
 end
